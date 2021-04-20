a70b0e278ada7c0e594dd1f1d616e732235fc1bd
enel <shang1jk@gmail.com>
2021-04-20 18:26:51 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ java/src/leetcode/_3_findrepeatnumber/FindRepeatNumber.java
@@ -0,0 +1,40 @@
+package leetcode._3_findrepeatnumber;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/">剑指 Offer 03. 数组中重复的数字</a>
+ */
+public class FindRepeatNumber {
+    public static class Solution1 {
+        public int findRepeatNumber(int[] nums) {
+            if (nums == null) return -1;
+            Set set = new HashSet<Integer>();
+            for (int n : nums) {
+                if (!set.add(n)) {
+                    return n;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static class Solution2 {
+        public int findRepeatNumber(int[] nums) {
+            int tmp;
+            for (int i = 0; i < nums.length; i++) {
+                while (nums[i] != i) {
+                    if (nums[i] == nums[nums[i]]) {
+                        return nums[i];
+                    }
+                    tmp = nums[i];
+                    nums[i] = nums[tmp];
+                    nums[tmp] = tmp;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
---
+++ java/src/leetcode/_3_findrepeatnumber/FindRepeatNumberTest.java
@@ -0,0 +1,19 @@
+package leetcode._3_findrepeatnumber;
+
+import leetcode._3_findrepeatnumber.FindRepeatNumber.Solution1;
+import leetcode._3_findrepeatnumber.FindRepeatNumber.Solution2;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindRepeatNumberTest {
+
+    @Test
+    public void testFindRepeatNumber() {
+        int[] nums   = new int[]{2, 3, 1, 0, 2, 5, 3};
+        int   result = new Solution1().findRepeatNumber(nums);
+        Assert.assertTrue(result == 2 || result == 3);
+
+        result = new Solution2().findRepeatNumber(nums);
+        Assert.assertTrue(result == 2 || result == 3);
+    }
+}
