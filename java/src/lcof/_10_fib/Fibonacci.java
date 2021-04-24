78f0038869cf06ab82099ad185e8b2b21baf3ffd
enel <shang1jk@gmail.com>
2021-04-24 20:12:17 +0800

    剑指 Offer 10- I. 斐波那契数列

---
+++ java/src/lcof/_10_fib/Fibonacci.java
@@ -0,0 +1,26 @@
+package lcof._10_fib;
+
+public class Fibonacci {
+
+    public static class Solution1 {
+        public int fib(int n) {
+            int a = 0, b = 1;
+            int tmp;
+            for (int i = 0; i < n; i++) {
+                tmp = a + b;
+                a = b;
+                b = tmp;
+            }
+            return a;
+        }
+    }
+
+    public static class Solution2 {
+        public int fib(int n) {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return fib(--n) + fib(--n);
+        }
+    }
+
+}
---
+++ java/src/lcof/_10_fib/FibonacciTest.java
@@ -0,0 +1,20 @@
+package lcof._10_fib;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciTest {
+    @Test
+    public void testSolution1() {
+        Fibonacci.Solution1 solution1 = new Fibonacci.Solution1();
+        Assert.assertEquals(1, solution1.fib(2));
+        Assert.assertEquals(5, solution1.fib(5));
+    }
+
+    @Test
+    public void testSolution2() {
+        Fibonacci.Solution2 solution2 = new Fibonacci.Solution2();
+        Assert.assertEquals(1, solution2.fib(2));
+        Assert.assertEquals(5, solution2.fib(5));
+    }
+}
