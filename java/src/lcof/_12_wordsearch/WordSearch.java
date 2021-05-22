17c2c5fdc01742aa67af67a1dde94f9ed58ef18c
enel <shang1jk@gmail.com>
2021-05-22 10:15:35 +0800

    剑指 Offer 12. 矩阵中的路径

--- java/src/lcof/_12_wordsearch/WordSearch.java
+++ java/src/lcof/_12_wordsearch/WordSearch.java
@@ -14,7 +14,7 @@ public class WordSearch {
         return false;
     }
 
-    public boolean search(char[][] board, int i, int j, String word, int k) {
+    private boolean search(char[][] board, int i, int j, String word, int k) {
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(k)) return false;
         if (k == word.length() - 1) return true;
         board[i][j] = '\0';
